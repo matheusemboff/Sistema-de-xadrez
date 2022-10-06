6c534a9f9d1c6736b2fd154220d63a036ca6c171
Matheus Boff <matheusmedeirosboff@hotmail.com>
2022-10-06 15:34:53 -0300

    First Class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+import Boardgame.Position;
+
+/**
+ *
+ * @author matheus.boff
+ */
+public class Program {
+    public static void main (String args[]){
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+        
+    }
+}
